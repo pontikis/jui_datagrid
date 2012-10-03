6e1c3ddd75662b7bf2c333ef1373217664f55854
Christos Pontikis <pontikis@gmail.com>
2012-10-03 22:55:58 +0300

    Plugin design pattern  - ref #3

--- demo.js
+++ demo.js
@@ -3,4 +3,22 @@ $(function() {
 
     $("#demo_grid1").jui_datagrid({
     });
+
+
+    $("#set_UI_style").click(function() {
+
+        var table_class = 'ui-styled-table';
+        var tr_hover_class = 'ui-state-hover';
+        var th_class = 'ui-state-default';
+        var td_class = 'ui-widget-content';
+        var tr_last_class = 'last-child';
+
+        $("#demo_grid1").jui_datagrid('setUIStyle', 'dg_demo_grid1', table_class, tr_hover_class, th_class, td_class, tr_last_class);
+    });
+
+
+    $("#get_option").click(function() {
+        alert($("#demo_grid1").jui_datagrid('setUIStyle'));
+    });
+
 });
--- jquery.jui_datagrid.js
+++ jquery.jui_datagrid.js
@@ -7,24 +7,44 @@
  * License GPLv3
  */
 (function($) {
-    $.fn.jui_datagrid = function(options) {
 
-        // Create some defaults, extending them with any options that were provided
-        var settings = $.extend( {
-            table_id_prefix: 'dg_',
-            table_class: 'ui-styled-table',
-            tr_hover_class: 'ui-state-hover',
-            th_class: 'ui-state-default',
-            td_class: 'ui-widget-content',
-            tr_last_class: 'last-child'
-        }, options);
+    var methods = {
+        init: function(options) {
+            // Create some defaults, extending them with any options that were provided
+            var settings = $.extend({
+                table_id_prefix: 'dg_',
+                apply_UI_style: true,
+                table_class: 'ui-styled-table',
+                tr_hover_class: 'ui-state-hover',
+                th_class: 'ui-state-default',
+                td_class: 'ui-widget-content',
+                tr_last_class: 'last-child'
+            }, options);
 
-        /**
-         * @param table_id
-         * @param table_class
-         * @link http://stackoverflow.com/questions/2613632/jquery-ui-themes-and-html-tables
-         */
-        function ui_style_table(table_id, table_class, tr_hover_class, th_class, td_class, tr_last_class) {
+            return this.each(function() {
+                var elem = $(this);
+                var container_id = $(this).attr("id");
+                var table_id = settings.table_id_prefix + container_id;
+
+                var tbl = '<table id="' + table_id + '">' +
+                    '<thead>' +
+                    '<tr><th>Header Column</th><th>Column 2</th><th>Column 3</th><th>Column 4</th></tr>' +
+                    '</thead>' +
+                    '<tbody>' +
+                    '<tr><th>Header</th><td>Cell 2</td><td>Cell 3</td><td>Cell 4</td></tr>' +
+                    '<tr><th>Header</th><td>Cell 2</td><td>Cell 3</td><td>Cell 4</td></tr>' +
+                    '<tr><th>Header</th><td>Cell 2</td><td>Cell 3</td><td>Cell 4</td></tr>' +
+                    '</tbody>' +
+                    '</table>';
+
+                elem.html(tbl);
+                if(settings.apply_UI_style) {
+                    elem.jui_datagrid('setUIStyle', table_id, settings.table_class, settings.tr_hover_class, settings.th_class, settings.td_class, settings.tr_last_class);
+                }
+
+            });
+        },
+        setUIStyle: function(table_id, table_class, tr_hover_class, th_class, td_class, tr_last_class) {
             var table_selector = '#' + table_id;
             $(table_selector).addClass(table_class);
 
@@ -38,29 +58,19 @@
             $(table_selector).find("td").addClass(td_class);
             $(table_selector).find("tr:last-child").addClass(tr_last_class);
         }
+    };
 
+    $.fn.jui_datagrid = function(method) {
 
-        return this.each(function() {
-            obj = $(this);
-            var container_id = $(this).attr("id");
-            var table_id = settings.table_id_prefix + container_id;
-
-            var tbl = '<table id="' + table_id + '">' +
-                '<thead>' +
-                '<tr><th>Header Column</th><th>Column 2</th><th>Column 3</th><th>Column 4</th></tr>' +
-                '</thead>' +
-                '<tbody>' +
-                '<tr><th>Header</th><td>Cell 2</td><td>Cell 3</td><td>Cell 4</td></tr>' +
-                '<tr><th>Header</th><td>Cell 2</td><td>Cell 3</td><td>Cell 4</td></tr>' +
-                '<tr><th>Header</th><td>Cell 2</td><td>Cell 3</td><td>Cell 4</td></tr>' +
-                '</tbody>' +
-                '</table>';
-
-            obj.html(tbl);
-
-            ui_style_table(table_id, settings.table_class, settings.tr_hover_class, settings.th_class, settings.td_class, settings.tr_last_class);
-
-        });
+        // Method calling logic
+        if(methods[method]) {
+            return methods[ method ].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if(typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.jui_datagrid');
+        }
 
     };
+
 })(jQuery);
