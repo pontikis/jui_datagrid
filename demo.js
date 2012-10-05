84f2c0e56dfa173f1f21ca12833eea5651c53dfa
Christos Pontikis <pontikis@gmail.com>
2012-10-05 21:53:01 +0300

    Plugin design pattern (private and public methods, events, options stored to elem.data)  - close #3

--- demo.js
+++ demo.js
@@ -2,7 +2,17 @@ $(function() {
     //$('#switcher').themeswitcher();
 
     $("#demo_grid1").jui_datagrid({
+        apply_UI_style: false
+/*        onShow: function(msg) {
+            alert('Hello world! ' + msg);
+        }*/
     });
+
+/*    $('#demo_grid1').jui_datagrid({
+        onShow: function(msg) {
+            alert('Hello world! ' + msg);
+        }
+    });*/
 
 
     $("#set_UI_style").click(function() {
@@ -13,12 +23,18 @@ $(function() {
         var td_class = 'ui-widget-content';
         var tr_last_class = 'last-child';
 
-        $("#demo_grid1").jui_datagrid('setUIStyle', 'dg_demo_grid1', table_class, tr_hover_class, th_class, td_class, tr_last_class);
+        $("#demo_grid1").jui_datagrid('setUIStyle', table_class, tr_hover_class, th_class, td_class, tr_last_class);
     });
 
 
     $("#get_option").click(function() {
-        alert($("#demo_grid1").jui_datagrid('setUIStyle'));
+        var test1 = $("#demo_grid1").jui_datagrid('getOption', 'tr_last_class');
+        alert(test1);
     });
+
+    $("#destroy").click(function() {
+        $("#demo_grid1").jui_datagrid('destroy');
+    });
+
 
 });
--- jquery.jui_datagrid.js
+++ jquery.jui_datagrid.js
@@ -8,22 +8,44 @@
  */
 (function($) {
 
+    var pluginName = 'jui_datagrid';
+
+    // private methods
+
+    // public methods
     var methods = {
+
+        /**
+         * @constructor
+         * @param options
+         * @return {*}
+         */
         init: function(options) {
-            // Create some defaults, extending them with any options that were provided
-            var settings = $.extend({
-                table_id_prefix: 'dg_',
-                apply_UI_style: true,
-                table_class: 'ui-styled-table',
-                tr_hover_class: 'ui-state-hover',
-                th_class: 'ui-state-default',
-                td_class: 'ui-widget-content',
-                tr_last_class: 'last-child'
-            }, options);
+
+            var elem = this;
 
             return this.each(function() {
-                var elem = $(this);
-                var container_id = $(this).attr("id");
+
+                var settings = elem.data(pluginName);
+                if(typeof(settings) == 'undefined') {
+                    var defaults = {
+                        table_id_prefix: 'dg_',
+                        apply_UI_style: true,
+                        table_class: 'ui-styled-table',
+                        tr_hover_class: 'ui-state-hover',
+                        th_class: 'ui-state-default',
+                        td_class: 'ui-widget-content',
+                        tr_last_class: 'last-child',
+                        onShow: function() {
+                        }
+                    };
+                    settings = $.extend({}, defaults, options);
+                    elem.data(pluginName, settings);
+                } else {
+                    settings = $.extend({}, settings, options);
+                }
+
+                var container_id = elem.attr("id");
                 var table_id = settings.table_id_prefix + container_id;
 
                 var tbl = '<table id="' + table_id + '">' +
@@ -39,13 +61,39 @@
 
                 elem.html(tbl);
                 if(settings.apply_UI_style) {
-                    elem.jui_datagrid('setUIStyle', table_id, settings.table_class, settings.tr_hover_class, settings.th_class, settings.td_class, settings.tr_last_class);
+                    elem.jui_datagrid('setUIStyle', settings.table_class, settings.tr_hover_class, settings.th_class, settings.td_class, settings.tr_last_class);
                 }
+
+                // bind event
+                elem.bind("onShow", settings.onShow);
+                // trigger event
+                elem.triggerHandler("onShow");
 
             });
         },
-        setUIStyle: function(table_id, table_class, tr_hover_class, th_class, td_class, tr_last_class) {
-            var table_selector = '#' + table_id;
+
+        /**
+         * Get any option set to plugin using its name (as string)
+         * Usage: $(element).jui_datagrid('getOption', some_option);
+         * @param opt
+         * @return {*}
+         */
+        getOption: function(opt) {
+            var elem = $(this);
+            return elem.data(pluginName)[opt];
+        },
+
+        /**
+         * Apply UI styles to datagrid table
+         * @param table_class
+         * @param tr_hover_class
+         * @param th_class
+         * @param td_class
+         * @param tr_last_class
+         */
+        setUIStyle: function(table_class, tr_hover_class, th_class, td_class, tr_last_class) {
+            var elem = $(this);
+            var table_selector = '#' + $(elem).jui_datagrid('getOption', 'table_id_prefix') + elem.attr("id");
             $(table_selector).addClass(table_class);
 
             if(tr_hover_class) {
@@ -57,6 +105,19 @@
             $(table_selector).find("th").addClass(th_class);
             $(table_selector).find("td").addClass(td_class);
             $(table_selector).find("tr:last-child").addClass(tr_last_class);
+        },
+
+        /**
+         * Destroy plugin
+         * @param options
+         * @return {*|jQuery}
+         */
+        destroy: function(options) {
+            return $(this).each(function() {
+                var $this = $(this);
+
+                $this.removeData(pluginName);
+            });
         }
     };
 
@@ -68,7 +129,7 @@
         } else if(typeof method === 'object' || !method) {
             return methods.init.apply(this, arguments);
         } else {
-            $.error('Method ' + method + ' does not exist on jQuery.jui_datagrid');
+            $.error('Method ' + method + ' does not exist on jQuery.' + pluginName);
         }
 
     };
