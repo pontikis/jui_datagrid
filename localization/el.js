90ae2b615dfd7a5cbd6c924871ab90ff6abc65a8
Christos Pontikis <pontikis@gmail.com>
2012-10-24 12:44:20 +0300

    Localization

---
+++ localization/el.js
@@ -0,0 +1,10 @@
+/**
+ * jui_datagrid simple localization - GREEK
+ *
+ * DO NOT CHANGE this file, as it will be overwritten in next update.
+ * To use different values, write and use a similar structure js file.
+ *
+ */
+var rsc_jui_dg = {
+    test: 'test'
+};
