d3c7d7c57653394751f9822565d980a0eabe2399
Christos Pontikis <pontikis@gmail.com>
2012-11-17 16:46:06 +0200

    sync_thead_tbody_column_width() function

--- jquery.jui_datagrid.js
+++ jquery.jui_datagrid.js
@@ -878,8 +878,8 @@
 
         var elem = $("#" + container_id),
 
-            header_table_selector = '#' + create_id($(elem).jui_datagrid('getOption', 'header_table_id_prefix'), container_id),
-            data_table_selector = '#' + create_id($(elem).jui_datagrid('getOption', 'table_id_prefix'), container_id),
+            header_table_selector = '#' + create_id(elem.jui_datagrid('getOption', 'header_table_id_prefix'), container_id),
+            data_table_selector = '#' + create_id(elem.jui_datagrid('getOption', 'table_id_prefix'), container_id),
             elem_header_table = $(header_table_selector),
             elem_data_table = $(data_table_selector),
 
@@ -922,71 +922,17 @@
             headerClass = columns[i]['headerClass'];
             if(headerClass !== "") {
                 $(header_table_selector + ' th').eq(col).addClass(headerClass);
-                //console.log(header_table_selector + '_col_' + col);
-                //console.log(headerClass);
             }
-        }
-
-        for(i in columns) {
-            col = showRowIndex ? parseInt(i) + 1 : parseInt(i);
             dataClass = columns[i]['dataClass'];
             if(dataClass !== "") {
                 $(data_table_selector + ' tr').each(function() {
                     $(this).find("td").eq(col).addClass(dataClass);
                 });
-                //console.log(data_table_selector + '_col_' + col);
-                //console.log(dataClass);
             }
         }
 
+        sync_thead_tbody_column_width(container_id);
 
-        // try to detect id header columns width need syncronization with data table columns
-        var cols = $(header_table_selector + ' th').length,
-            cols_except_last = cols - 1,
-            th_cow, td_first_row_cow, // cow = computed outer width
-            fix_columns_width_needed = false;
-        for(i = 0; i < cols_except_last; i++) {
-            th_cow = $(header_table_selector + ' th').eq(i).width();
-            td_first_row_cow = $(data_table_selector).find("tr").eq(0).find("td").eq(i).width();
-            if(th_cow !== td_first_row_cow) {
-                fix_columns_width_needed = true;
-                break;
-            }
-        }
-
-        if(fix_columns_width_needed) {
-
-            // sync header and data tables column width ----------------------------
-            var cw, cw_min, elem_th, elem_td, // cw = computed width
-                a_col_cw = [],
-                fix_td_width = parseInt(elem.data(pluginStatus)['fix_border_collapse_td_width']);
-
-            // define column min-widths
-            $(header_table_selector).addClass("shrink");
-            for(i = 0; i < cols; i++) {
-                elem_th = $(header_table_selector).find("tr").eq(0).find("th").eq(i);
-                cw_min = elem_th.width();
-
-                elem_td = $(data_table_selector).find("tr").eq(0).find("td").eq(i);
-                elem_td.css("min-width", cw_min + 'px');
-            }
-            $(header_table_selector).removeClass("shrink");
-
-            // apply first tr td widths to header
-            for(i = 0; i < cols_except_last; i++) {
-                var elem_cur = $(data_table_selector).find("tr").eq(0).find("td").eq(i);
-                cw = elem_cur.width() + fix_td_width;
-                a_col_cw.push(cw);
-                $(header_table_selector + ' th').eq(i).width(cw);
-            }
-
-            // re-apply first tr td widths to data table
-            for(i = 0; i < cols_except_last; i++) {
-                $(data_table_selector + ' tr').eq(0).find("td").eq(i).width(a_col_cw[i]);
-                //console.log(a_col_cw[i]);
-            }
-
-        }
     };
 
 
@@ -1409,6 +1355,65 @@
             label: rsc_jui_dg.tb_selected_label + ': ' + selected_rows
         });
     };
+
+    /**
+     *
+     * @param container_id
+     */
+    var sync_thead_tbody_column_width = function(container_id) {
+
+        var elem = $("#" + container_id),
+            header_table_selector = '#' + create_id(elem.jui_datagrid('getOption', 'header_table_id_prefix'), container_id),
+            data_table_selector = '#' + create_id(elem.jui_datagrid('getOption', 'table_id_prefix'), container_id),
+            cols = $(header_table_selector + ' th').length,
+            cols_except_last = cols - 1,
+            i, th_cow, td_first_row_cow, // cow = computed outer width
+            fix_columns_width_needed = false,
+
+            cw, cw_min, elem_th, elem_td, // cw = computed width
+            a_col_cw = [],
+            fix_td_width = parseInt(elem.data(pluginStatus)['fix_border_collapse_td_width']);
+
+
+        // try to detect id header columns width need syncronization with data table columns
+        for(i = 0; i < cols_except_last; i++) {
+            th_cow = $(header_table_selector + ' th').eq(i).width();
+            td_first_row_cow = $(data_table_selector).find("tr").eq(0).find("td").eq(i).width();
+            if(th_cow !== td_first_row_cow) {
+                fix_columns_width_needed = true;
+                break;
+            }
+        }
+
+        if(fix_columns_width_needed) {
+
+            // define column min-widths
+            $(header_table_selector).addClass("shrink");
+            for(i = 0; i < cols; i++) {
+                elem_th = $(header_table_selector).find("tr").eq(0).find("th").eq(i);
+                cw_min = elem_th.width();
+
+                elem_td = $(data_table_selector).find("tr").eq(0).find("td").eq(i);
+                elem_td.css("min-width", cw_min + 'px');
+            }
+            $(header_table_selector).removeClass("shrink");
+
+            // apply first tr td widths to header
+            for(i = 0; i < cols_except_last; i++) {
+                var elem_cur = $(data_table_selector).find("tr").eq(0).find("td").eq(i);
+                cw = elem_cur.width() + fix_td_width;
+                a_col_cw.push(cw);
+                $(header_table_selector + ' th').eq(i).width(cw);
+            }
+
+            // re-apply first tr td widths to data table
+            for(i = 0; i < cols_except_last; i++) {
+                $(data_table_selector + ' tr').eq(0).find("td").eq(i).width(a_col_cw[i]);
+            }
+
+        }
+    };
+
 
 
     $.fn.jui_datagrid = function(method) {
