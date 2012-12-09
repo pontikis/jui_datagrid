35556be23248b777a1087fc44359ae05b2202945
Christos Pontikis <pontikis@gmail.com>
2012-12-09 22:01:00 +0200

    demo database sql

--- demo/mysql/jui_datagrid.sql
+++ demo/mysql/jui_datagrid.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.27, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.28, for Linux (x86_64)
 --
 -- Host: localhost    Database: jui_datagrid
 -- ------------------------------------------------------
--- Server version	5.5.27
+-- Server version	5.5.28
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -48,6 +48,29 @@ INSERT INTO `customers` VALUES (1,'Mullins','Elton',1,'nec@quamPellentesque.org'
 UNLOCK TABLES;
 
 --
+-- Table structure for table `lk_countries`
+--
+
+DROP TABLE IF EXISTS `lk_countries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lk_countries` (
+  `iso_code` varchar(20) NOT NULL,
+  `country` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lk_countries`
+--
+
+LOCK TABLES `lk_countries` WRITE;
+/*!40000 ALTER TABLE `lk_countries` DISABLE KEYS */;
+INSERT INTO `lk_countries` VALUES ('AF','Afghanistan'),('AX','Aland Islands'),('AL','Albania'),('DZ','Algeria'),('AS','American Samoa'),('AD','Andorra'),('AO','Angola'),('AI','Anguilla'),('AQ','Antarctica'),('AG','Antigua and Barbuda'),('AR','Argentina'),('AM','Armenia'),('AW','Aruba'),('AC','Ascension Island'),('AU','Australia'),('AT','Austria'),('AZ','Azerbaijan'),('BS','Bahamas'),('BH','Bahrain'),('BB','Barbados'),('BD','Bangladesh'),('BY','Belarus'),('BE','Belgium'),('BZ','Belize'),('BJ','Benin'),('BM','Bermuda'),('BT','Bhutan'),('BW','Botswana'),('BO','Bolivia'),('BA','Bosnia and Herzegovina'),('BV','Bouvet Island'),('BR','Brazil'),('IO','British Indian Ocean Territory'),('BN','Brunei Darussalam'),('BG','Bulgaria'),('BF','Burkina Faso'),('BI','Burundi'),('KH','Cambodia'),('CM','Cameroon'),('CA','Canada'),('CV','Cape Verde'),('KY','Cayman Islands'),('CF','Central African Republic'),('TD','Chad'),('CL','Chile'),('CN','China'),('CX','Christmas Island'),('CC','Cocos (Keeling) Islands'),('CO','Colombia'),('KM','Comoros'),('CG','Congo'),('CD','Congo, Democratic Republic'),('CK','Cook Islands'),('CR','Costa Rica'),('CI','Cote D\'Ivoire (Ivory Coast)'),('HR','Croatia (Hrvatska)'),('CU','Cuba'),('CY','Cyprus'),('CZ','Czech Republic'),('CS','Czechoslovakia (former)'),('DK','Denmark'),('DJ','Djibouti'),('DM','Dominica'),('DO','Dominican Republic'),('TP','East Timor'),('EC','Ecuador'),('EG','Egypt'),('SV','El Salvador'),('GQ','Equatorial Guinea'),('ER','Eritrea'),('EE','Estonia'),('ET','Ethiopia'),('EU','European Union'),('FK','Falkland Islands (Malvinas)'),('FO','Faroe Islands'),('FJ','Fiji'),('FI','Finland'),('FR','France'),('FX','France, Metropolitan'),('GF','French Guiana'),('PF','French Polynesia'),('TF','French Southern Territories'),('MK','F.Y.R.O.M. (Macedonia)'),('GA','Gabon'),('GM','Gambia'),('GE','Georgia'),('DE','Germany'),('GH','Ghana'),('GI','Gibraltar'),('GB','Great Britain (UK)'),('GR','Greece'),('GL','Greenland'),('GD','Grenada'),('GP','Guadeloupe'),('GU','Guam'),('GT','Guatemala'),('GG','Guernsey'),('GN','Guinea'),('GW','Guinea-Bissau'),('GY','Guyana'),('HT','Haiti'),('HM','Heard and McDonald Islands'),('HN','Honduras'),('HK','Hong Kong'),('HU','Hungary'),('IS','Iceland'),('IN','India'),('ID','Indonesia'),('IR','Iran'),('IQ','Iraq'),('IE','Ireland'),('IL','Israel'),('IM','Isle of Man'),('IT','Italy'),('JE','Jersey'),('JM','Jamaica'),('JP','Japan'),('JO','Jordan'),('KZ','Kazakhstan'),('KE','Kenya'),('KI','Kiribati'),('KP','Korea (North)'),('KR','Korea (South)'),('XK','Kosovo*'),('KW','Kuwait'),('KG','Kyrgyzstan'),('LA','Laos'),('LV','Latvia'),('LB','Lebanon'),('LI','Liechtenstein'),('LR','Liberia'),('LY','Libya'),('LS','Lesotho'),('LT','Lithuania'),('LU','Luxembourg'),('MO','Macau'),('MG','Madagascar'),('MW','Malawi'),('MY','Malaysia'),('MV','Maldives'),('ML','Mali'),('MT','Malta'),('MH','Marshall Islands'),('MQ','Martinique'),('MR','Mauritania'),('MU','Mauritius'),('YT','Mayotte'),('MX','Mexico'),('FM','Micronesia'),('MC','Monaco'),('MD','Moldova'),('MN','Mongolia'),('ME','Montenegro'),('MS','Montserrat'),('MA','Morocco'),('MZ','Mozambique'),('MM','Myanmar'),('NA','Namibia'),('NR','Nauru'),('NP','Nepal'),('NL','Netherlands'),('AN','Netherlands Antilles'),('NT','Neutral Zone'),('NC','New Caledonia'),('NZ','New Zealand (Aotearoa)'),('NI','Nicaragua'),('NE','Niger'),('NG','Nigeria'),('NU','Niue'),('NF','Norfolk Island'),('MP','Northern Mariana Islands'),('NO','Norway'),('OM','Oman'),('PK','Pakistan'),('PW','Palau'),('PS','Palestinian Territory, Occupied'),('PA','Panama'),('PG','Papua New Guinea'),('PY','Paraguay'),('PE','Peru'),('PH','Philippines'),('PN','Pitcairn'),('PL','Poland'),('PT','Portugal'),('PR','Puerto Rico'),('QA','Qatar'),('RE','Reunion'),('RO','Romania'),('RU','Russian Federation'),('RW','Rwanda'),('GS','S. Georgia and S. Sandwich Isls.'),('SH','Saint Helena'),('KN','Saint Kitts and Nevis'),('LC','Saint Lucia'),('MF','Saint Martin'),('VC','Saint Vincent & the Grenadines'),('WS','Samoa'),('SM','San Marino'),('ST','Sao Tome and Principe'),('SA','Saudi Arabia'),('SN','Senegal'),('RS','Serbia'),('YU','Serbia and Montenegro (former)'),('SC','Seychelles'),('SL','Sierra Leone'),('SG','Singapore'),('SI','Slovenia'),('SK','Slovak Republic'),('SB','Solomon Islands'),('SO','Somalia'),('ZA','South Africa'),('SS','South Sudan'),('ES','Spain'),('LK','Sri Lanka'),('SD','Sudan'),('SR','Suriname'),('SJ','Svalbard & Jan Mayen Islands'),('SZ','Swaziland'),('SE','Sweden'),('CH','Switzerland'),('SY','Syria'),('TW','Taiwan'),('TJ','Tajikistan'),('TZ','Tanzania'),('TH','Thailand'),('TG','Togo'),('TK','Tokelau'),('TO','Tonga'),('TT','Trinidad and Tobago'),('TN','Tunisia'),('TR','Turkey'),('TM','Turkmenistan'),('TC','Turks and Caicos Islands'),('TV','Tuvalu'),('UG','Uganda'),('UA','Ukraine'),('AE','United Arab Emirates'),('UK','United Kingdom'),('US','United States'),('UM','US Minor Outlying Islands'),('UY','Uruguay'),('SU','USSR (former)'),('UZ','Uzbekistan'),('VU','Vanuatu'),('VA','Vatican City State (Holy See)'),('VE','Venezuela'),('VN','Viet Nam'),('VG','British Virgin Islands'),('VI','Virgin Islands (U.S.)'),('WF','Wallis and Futuna Islands'),('EH','Western Sahara'),('YE','Yemen'),('ZM','Zambia'),('ZR','Zaire'),('ZW','Zimbabwe');
+/*!40000 ALTER TABLE `lk_countries` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `lk_genders`
 --
 
@@ -83,4 +106,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-10-06 12:00:06
+-- Dump completed on 2012-12-09 18:25:10
