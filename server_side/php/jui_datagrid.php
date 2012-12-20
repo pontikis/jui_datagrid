f32c8bec60c845b4964bc9a41f4ff05ac1fd774f
Christos Pontikis <pontikis@gmail.com>
2012-12-20 18:13:16 +0200

    jui_datagrid php class

--- server_side/php/jui_datagrid.php
+++ server_side/php/jui_datagrid.php
@@ -4,28 +4,38 @@
  **/
 class jui_datagrid {
 
-	/**
-	 * @var object database connection
-	 */
+	/** @var object database connection */
 	var $conn;
-	/**
-	 * @var bool use prepared statements or not
-	 */
+	/** @var bool use prepared statements or not */
 	var $usePreparedStatements;
-	/**
-	 * @var string rdbms in use (one of "ADODB", "MYSQL", "MYSQLi", "PDO", "POSTGRES")
-	 */
+	/** @var string rdbms in use (one of "ADODB", "MYSQL", "MYSQLi", "PDO", "POSTGRES") */
 	var $rdbms;
+
+	var $page_num;
+
+	var $rows_per_page;
+
+	var $sorting;
+
+	var $filter_rules;
 
 	/**
 	 * @param object $dbcon database connection
 	 * @param bool $use_ps use prepared statements or not
 	 * @param string $db_type rdbms in use (one of "ADODB", "MYSQL", "MYSQLi", "PDO", "POSTGRES")
+	 * @param $page_num
+	 * @param $rows_per_page
+	 * @param $sorting
+	 * @param $filter_rules
 	 */
-	public function __construct($dbcon, $use_ps, $db_type) {
+	public function __construct($dbcon, $use_ps, $db_type, $page_num, $rows_per_page, $sorting, $filter_rules) {
 		$this->conn = $dbcon;
 		$this->usePreparedStatements = $use_ps;
 		$this->rdbms = $db_type;
+		$this->page_num = $page_num;
+		$this->rows_per_page = $rows_per_page;
+		$this->sorting = $sorting;
+		$this->filter_rules = $filter_rules;
 	}
 
 	public function set_conn($dbcon) {
@@ -53,38 +63,31 @@ class jui_datagrid {
 	}
 
 
+	public function get_total_rows() {
+		$total_rows = 0;
+		switch($this->rdbms) {
+			case "ADODB":
+				break;
+			default:
+				/**  \todo get total rows using other rdbms */
+				$total_rows = false;
+		}
 
-
-	public function get_total_rows_ADODB() {
-
+		return $total_rows;
 	}
 
 
+	public function fetch_page_data() {
 
-
-	public function fetch_page_data_ADODB() {
-
-	}
-
-	/**
-	 * \todo fetch page data using PostgreSQL
-	 */
-	public function fetch_page_data_POSTGRES() {
-
-	}
-
-	/**
-	 * \todo fetch page data using MySQL
-	 */
-	public function fetch_page_data_MYSQL() {
+		switch($this->rdbms) {
+			case "ADODB":
+				break;
+			default:
+				/**  \todo fetch page data () using other rdbms */
+				return false;
+		}
 
 	}
 
-	/**
-	 * \todo fetch page data using MySQLi
-	 */
-	public function fetch_page_data_MYSQLi() {
-
-	}
 
 }
