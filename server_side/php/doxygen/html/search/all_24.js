7600d99bdb139fe8e78ab95d31c0ec72ff6090f8
Christos Pontikis <pontikis@gmail.com>
2012-12-22 20:35:10 +0200

    Update docs

--- server_side/php/doxygen/html/search/all_24.js
+++ server_side/php/doxygen/html/search/all_24.js
@@ -1,5 +1,18 @@
 var searchData=
 [
+  ['_24a_5fdata',['$a_data',['../d7/dd6/fetch__data_8php.html#ade8f17dc8d74630899faaf17472391cd',1,'fetch_data.php']]],
+  ['_24bind_5fparams',['$bind_params',['../d7/dd6/fetch__data_8php.html#a4787a76724552b9621cb0250ceb29e8d',1,'fetch_data.php']]],
+  ['_24conn',['$conn',['../d7/dd6/fetch__data_8php.html#aa8a5a87b9c1a6a0819b88447cbe41877',1,'fetch_data.php']]],
   ['_24db_5fsettings',['$db_settings',['../de/d43/classjui__datagrid.html#ac406521d0a3c1cdcf1766c5a8b77d904',1,'jui_datagrid']]],
-  ['_24last_5ferror',['$last_error',['../de/d43/classjui__datagrid.html#a60375900d6ade93e662c348772867d64',1,'jui_datagrid']]]
+  ['_24dbcon_5fsettings',['$dbcon_settings',['../de/dc4/settings_8dist_8php.html#a37204861b65d4d87e1b9d450d76539a0',1,'settings.dist.php']]],
+  ['_24filter_5frules',['$filter_rules',['../d7/dd6/fetch__data_8php.html#abaadef9a387318e61f818d346abc5f96',1,'fetch_data.php']]],
+  ['_24jdg',['$jdg',['../d7/dd6/fetch__data_8php.html#a3b11ebb3f9af216d2f24df42bf251fa2',1,'fetch_data.php']]],
+  ['_24json',['$json',['../d7/dd6/fetch__data_8php.html#ad91ed2c465f0491fa2bf60f0bd9a4987',1,'fetch_data.php']]],
+  ['_24last_5ferror',['$last_error',['../de/d43/classjui__datagrid.html#a60375900d6ade93e662c348772867d64',1,'jui_datagrid\$last_error()'],['../d7/dd6/fetch__data_8php.html#a60375900d6ade93e662c348772867d64',1,'$last_error():&#160;fetch_data.php']]],
+  ['_24page_5fnum',['$page_num',['../d7/dd6/fetch__data_8php.html#a129f576e647018fa748a3180db6bd78d',1,'fetch_data.php']]],
+  ['_24result',['$result',['../d7/dd6/fetch__data_8php.html#a112ef069ddc0454086e3d1e6d8d55d07',1,'$result():&#160;fetch_data.php'],['../d7/dd6/fetch__data_8php.html#a0a5f262e6a3a4f5a135a85fad28d659e',1,'$result():&#160;fetch_data.php'],['../d7/dd6/fetch__data_8php.html#aec22d50073fe1293044cf30af45877e9',1,'$result():&#160;fetch_data.php'],['../d7/dd6/fetch__data_8php.html#ad06a3bf1cf2a541f96a2cf96f452efc4',1,'$result():&#160;fetch_data.php']]],
+  ['_24rows_5fper_5fpage',['$rows_per_page',['../d7/dd6/fetch__data_8php.html#a25e9fd990572851e590a4d5bed823f20',1,'fetch_data.php']]],
+  ['_24sorting',['$sorting',['../d7/dd6/fetch__data_8php.html#abdc5d7c5bae165869f15dbb990cd3b7c',1,'fetch_data.php']]],
+  ['_24total_5frows',['$total_rows',['../d7/dd6/fetch__data_8php.html#af2c641e024ee7eeec68ae26c08ca2cf2',1,'fetch_data.php']]],
+  ['_24wheresql',['$whereSQL',['../d7/dd6/fetch__data_8php.html#ac6ab311e7bbf2df03268f5deed72ab33',1,'fetch_data.php']]]
 ];
