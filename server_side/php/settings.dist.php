81ec4d6949619b94addef288dee4b84c62741bc5
Christos Pontikis <pontikis@gmail.com>
2012-12-23 22:19:46 +0200

    minor changes

--- server_side/php/settings.dist.php
+++ server_side/php/settings.dist.php
@@ -6,10 +6,10 @@ $dbcon_settings = array(
 	'php_adodb_dsn_options_persist' => '0', // do not change if you are not sure
 	'php_adodb_dsn_options_misc' => '', // do not set fetchmode here, it is set to ADODB_FETCH_ASSOC
 	'php_adodb_dsn_custom' => '',
-	'db_server' => 'MYSQL_SERVERNAME_OR_IP',
+	'db_server' => 'DB_SERVERNAME_OR_IP',
 	'db_name' => 'DBNAME',
-	'db_user' => 'MYSQL_USER',
-	'db_passwd' => 'MYSQL_PASSWORD',
+	'db_user' => 'DB_USER',
+	'db_passwd' => 'DB_PASSWORD',
 	'db_port' => '',
 	'query_after_connection' => '' // e.g. 'SET NAMES UTF8'
 );
