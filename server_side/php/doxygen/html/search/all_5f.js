a8062f17c0ad4c336d5539d43bc582b55c587271
Christos Pontikis <pontikis@gmail.com>
2012-12-25 17:49:43 +0200

    Update docs

--- server_side/php/doxygen/html/search/all_5f.js
+++ server_side/php/doxygen/html/search/all_5f.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['_5f_5fconstruct',['__construct',['../de/d43/classjui__datagrid.html#a095c5d389db211932136b53f25f39685',1,'jui_datagrid']]]
+  ['_5f_5fconstruct',['__construct',['../de/d43/classjui__datagrid.html#a4b4df025b7ca84ceb7a0ce8245714005',1,'jui_datagrid']]]
 ];
